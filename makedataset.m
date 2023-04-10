437061162dae8803b9b8aa5d6dedc53f6ecd99c0
junkozuka <sengoku_sal2000@yahoo.co.jp>
2023-04-10 18:19:13 +0900

    Add files via upload

---
+++ makedataset.m
@@ -0,0 +1,22 @@
+close all
+clear
+
+%%%% make dataset %%%%%%
+readme.dNum = 10; % number of data
+readme.dimSmp = 2; % dimension of sample
+readme.dimTrg = 1; % dimension of target
+
+dataset.sample = rand([readme.dimSmp, readme.dNum]);
+dataset.target = rand([readme.dimTrg, readme.dNum]);
+
+readme
+dataset
+
+
+%%% save dataset %%%%%
+
+%%% load dataset %%%%%
+
+%%% make readme %%%%%
+
+
